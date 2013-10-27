22b589e35a38f25e2d81992f9b6b95442cf3dcbf
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-10-27 00:11:27 -0600

    Everything broken

--- pdis.rb
+++ pdis.rb
@@ -6,6 +6,10 @@ class Player1
   def self.color
     [0, 0, 255]
   end
+
+  def self.to_s
+    "c"
+  end
 end
 
 class Player2
@@ -16,16 +20,20 @@ class Player2
   def self.color
     [220, 20, 60]
   end
+
+  def self.to_s
+    "a"
+  end
 end
 
 class Comparison
   def self.compare(move1, move2)
     if move1 == "cooperate" && move2 == "cooperate"
-      [1, 1]
+      [3, 3]
     elsif move1 == "not cooperate" && move2 == "cooperate"
-      [0, 3]
+      [5, 0]
     elsif move1 == "cooperate" && move2 == "not cooperate"
-      [3, 0]
+      [0, 5]
     else
       [2, 2]
     end
@@ -62,16 +70,28 @@ class Board
 
   def move(comparison)
     points_in_round = get_points_of_round(make_empty_board(0))
+    print(points_in_round, 5)
     reconfigure_board(points_in_round)
+  end
+
+  def print(board, padding)
+    (0..height-1).to_a.each_with_index do |row, i|
+      (0..width-1).to_a.each_with_index do |field, j|
+        Kernel.print(board[i][j].to_s.center(padding))
+      end
+      puts ""
+    end
+    puts ""
+    puts ""
   end
 
   private
 
   def make_empty_board(fill_value)
     result = []
-    (1..height).to_a.each_with_index do |row, i|
+    (0..height-1).to_a.each_with_index do |row, i|
       result << []
-      (1..width).to_a.each_with_index do |field, j|
+      (0..width-1).to_a.each_with_index do |field, j|
         result[i] << fill_value
       end
     end
@@ -79,8 +99,8 @@ class Board
   end
 
   def get_points_of_round(board_points)
-    (1..height).to_a.each_with_index do |row, i|
-      (1..width).to_a.each_with_index do |field, j|
+    (0..height-1).to_a.each_with_index do |row, i|
+      (0..width-1).to_a.each_with_index do |field, j|
         #play these against each other (right and down is what I play against)
         #i  , j+1
         #i+1, j-1
@@ -99,13 +119,13 @@ class Board
   def play_a_round(board_points, player1_position, player2_position)
     return unless is_on_board?(player1_position) && is_on_board?(player2_position)
 
-    player1 = @config[player1_position.first-1][player1_position.last-1]
-    player2 = @config[player2_position.first-1][player2_position.last-1]
+    player1 = @config[player1_position.first][player1_position.last]
+    player2 = @config[player2_position.first][player2_position.last]
 
     points_player1, points_player2 = Comparison.compare(player1.move, player2.move)
 
-    board_points[player1_position.first-1][player1_position.last-1] += points_player1
-    board_points[player2_position.first-1][player2_position.last-1] += points_player2
+    board_points[player1_position.first][player1_position.last] += points_player1
+    board_points[player2_position.first][player2_position.last] += points_player2
   end
 
   def is_on_board?(position)
@@ -116,8 +136,8 @@ class Board
   def reconfigure_board(board_points)
     new_config = make_empty_board(nil)
 
-    (1..height).to_a.each_with_index do |row, i|
-      (1..width).to_a.each_with_index do |field, j|
+    (0..height-1).to_a.each_with_index do |row, i|
+      (0..width-1).to_a.each_with_index do |field, j|
         new_config[i][j] = best_player_in_vicinity(board_points, [i, j])
       end
     end
@@ -126,12 +146,16 @@ class Board
   end
 
   def best_player_in_vicinity(board_points, position)
-    best_position_points = 99999
-    best_position = nil
+    p position
+    best_position = position
     (position.first-1..position.first+1).to_a.each do |i|
       (position.last-1..position.last+1).to_a.each do |j|
+        if position == [0,0]
+          p best_position
+          p [i, j, board_points[i][j], board_points[best_position.first, best_position.last]]
+        end
         next unless is_on_board?([i, j])
-        best_position = [i, j] if board_points[i][j] < best_position_points
+        best_position = [i, j] if board_points[i][j] > board_points[best_position.first, best_position.last]
       end
     end
     @config[best_position.first][best_position.last]
@@ -139,26 +163,32 @@ class Board
 end
 
 
-$b = Board.new(10, 20)
+$b = Board.new(20, 20)
 $b.random_distribution(Player1, Player2)
 
 
-Shoes.app(width: $b.width*10, height: $b.height*10) do
-  def paint_the_board
-    (1..($b.height)).to_a.each_with_index do |row, i|
-      (1..($b.width)).to_a.each_with_index do |field, j|
-        fill rgb(*$b.config[i][j].color)
-        rect(left: j * 10, top: i * 10, width: 10)
-      end
-    end
-  end
+#Shoes.app(width: $b.width*10, height: $b.height*10) do
+#  def paint_the_board
+#    (0..($b.height-1)).to_a.each_with_index do |row, i|
+#      (0..($b.width-1)).to_a.each_with_index do |field, j|
+#        fill rgb(*$b.config[i][j].color)
+#        rect(left: j * 10, top: i * 10, width: 10)
+#      end
+#    end
+#  end
+#
+#  animate(10) do |frame|
+#    if frame < 20
+#      $b.move(Comparison)
+#      paint_the_board
+#    end
+#  end
+#end
 
-  animate(10) do |frame|
-    if frame < 20
-      $b.move(Comparison)
-      paint_the_board
-    end
-  end
+
+$b.print($b.config, 4)
+
+10.times do
+  $b.move(Comparison)
+  $b.print($b.config, 4)
 end
-
-
